80437d53721004a5c4daf8a8651686638eaac00d
MarinaWyss <marina.bennett.wyss@gmail.com>
2019-10-05 10:52:10 +0200

    Improving code

--- PreProcessing.R
+++ PreProcessing.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(tools)
+library(stringr)
 
 # your filepath here
 path <- "/Users/marinabennett/Desktop/Hertie/1. Fall 2019/Machine Learning/Hertie-ML-TADA-Project/newspaper-data/English"
@@ -11,9 +13,15 @@ for (i in 1:length(filenamesList)) {
   load(filenamesList[i])
 }
 
-# make sure columns match
-## dropping columns that are only in some but not all, except topic_tags
-## will just add a topic_tags column to the two that are missing: Fox News and Infowars
+# creating a character vector of the dataframe names
+newsNames <- file_path_sans_ext(basename(filenamesList))
+newsNames <- word(newsNames, 1, sep = "_")
+newsNames <- paste0(newsNames, "_df")
+
+# rbind
+## make sure columns match
+### dropping columns that are only in some but not all, except topic_tags
+### will just add a topic_tags column to the two that are missing: Fox News and Infowars
 bbcnews_df <- bbcnews_df %>% select(-section)
 politico_df <- politico_df %>% select(-section)
 thehill_df <- thehill_df %>% select(-section)
@@ -24,30 +32,5 @@ cnbc_df <- cnbc_df %>% select(-news_keywords)
 foxnews_df$topic_tags <- "NA"
 infowars_df$topic_tags <- "NA"
 
-
-# rbind
-## This would be better if we could automatically create the list of all the dataframes vs. typing them out
-fullDataSet <- do.call("rbind", list(abcnews_df,
-                                     bbcnews_df,
-                                     bloomberg_df,
-                                     breitbart_df,
-                                     buzzfeed_df,
-                                     cnbc_df,
-                                     dailykos_df,
-                                     foxnews_df,
-                                     guardian_df,
-                                     huffingtonpost_df,
-                                     infowars_df,
-                                     motherjones_df,
-                                     newsweek_df,
-                                     newyorker_df,
-                                     nytimes_df,
-                                     politico_df,
-                                     thehill_df,
-                                     thinkprogress_df,
-                                     townhall_df,
-                                     usatoday_df,
-                                     vice_df,
-                                     washingtonpost_df,
-                                     wsj_df))
+fullDataSet <- do.call("rbind", lapply(newsNames, get))
 
