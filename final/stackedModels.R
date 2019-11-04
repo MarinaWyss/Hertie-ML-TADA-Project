3db890e136489879ca5e549b35417f374c393622
MarinaWyss <marina.bennett.wyss@gmail.com>
2019-11-04 11:42:55 +0100

    stacked models

--- final/stackedModels.R
+++ final/stackedModels.R
@@ -1,22 +1,8 @@
 ### Machine Learning Application of Demographic data on Gun Violence in U.S. ####
 library(caret)
-library(dplyr)    # for data wrangling
-library(ggplot2)  # for awesome graphics
-library(rsample)
-library(ranger)   # for a fast c++ implementation of random forest 
-library(h2o)
-library(dplyr)
 library(tidyverse)
-library(e1071)
-library(randomForest)
-library(mlbench)
-
-# h2o set-up 
-#h2o.no_progress()  # turn off h2o progress bars
-#h2o.init()         # launch h2o
-
-# your filepath here
-path <- "/Users/madelinebrady/Documents/GitHub/Hertie-ML-TADA-Project/final"
+library(h2o)
+h2o.init()
 
 # adding independent variables to data set
 fullDataSet <- read.csv("preppedDataSet.csv")
@@ -46,73 +32,59 @@ joinedDataSet <- left_join(fullDataSet, demographics) %>%
   fill(c(6:15), .direction = c("down"))
 str(joinedDataSet)
 
+# dropping not needed variables
+balancedData <- balancedData %>% 
+  select(-gamma)
 
-#downsampling the data 
-
-balancedData <- upSample(x = joinedDataSet[, -1],
+# downsampling the data 
+balancedData <- downSample(x = joinedDataSet[, -1],
                            y = joinedDataSet$topic)
 
-#split data into training and test 
-#using simple random sampling using caret package
-
-set.seed(123)  # for reproducibility
-index_3 <- createDataPartition(balancedData$topic, p = 0.7, 
+# split data into training and test 
+set.seed(123)  
+index <- createDataPartition(balancedData$topic, p = 0.7, 
                                list = FALSE)
-train.1 <- joinedDataSet[index_3, ]
-test.1  <- joinedDataSet[-index_3, ]
+trainDataNews <- balancedData[index, ]
+testDataNews  <- balancedData[-index, ]
 
-# view the first 6 rows of the downsampled, training data
-head(train.1)
+# stacked models
+Y = "topic"
+X <- setdiff(names(balancedData), Y)
 
-##document topic     gamma    outlet       date          location   zip
-#1    text2     1 0.3667261 breitbart 2018-10-01 Chicago, Illinois 60632
-#2   text78     1 0.4987770 breitbart 2018-10-01 Chicago, Illinois 60632
-#4   text41     2 0.9208401 breitbart 2018-10-01 Chicago, Illinois 60632
-#6 text4633     2 0.2815031   foxnews 2018-10-01 Chicago, Illinois 60632
-#7 text4636     2 0.8156081   foxnews 2018-10-01 Chicago, Illinois 60632
-#8 text4650     2 0.7551036   foxnews 2018-10-01 Chicago, Illinois 60632
-#medianIncome majorityRace countyVote dead injusted total children
-#1     3.496508            1          0    2        2     4        0
-#2     3.496508            1          0    2        2     4        0
-#4     3.496508            1          0    2        2     4        0
-#6     3.496508            1          0    2        2     4        0
-#7     3.496508            1          0    2        2     4        0
-#8     3.496508            1          0    2        2     4        0
-#murderSuicide
-#1             0
-#2             0
-#4             0
-#6             0
-#7             0
-#8             0
+train_h2o_news <- trainDataNews %>% 
+  as.h2o
 
-## first default random forest model using the caret package
+test_h2o_news <- testDataNews %>% 
+  as.h2o
 
-#cross-validaton, with 10 folds repeat 3 times
-control <- trainControl(method='repeatedcv', 
-                        number=10, 
-                        repeats=3)
+best_xgb_news <- h2o.xgboost(
+  x = X, y = Y, training_frame = train_h2o_news, ntrees = 5000, learn_rate = 0.05,
+  max_depth = 3, min_rows = 3, sample_rate = 0.8, categorical_encoding = "Enum",
+  nfolds = 10, fold_assignment = "Modulo", 
+  keep_cross_validation_predictions = TRUE, seed = 123, stopping_rounds = 50,
+  stopping_metric = "RMSE", stopping_tolerance = 0
+)
 
-#Metric compare model is Accuracy
-metric <- "Accuracy"
-set.seed(123)
+best_gbm_news <- h2o.gbm(
+  x = X, y = Y, training_frame = train_h2o_news, ntrees = 5000, learn_rate = 0.01,
+  max_depth = 7, min_rows = 5, sample_rate = 0.8, nfolds = 10,
+  fold_assignment = "Modulo", keep_cross_validation_predictions = TRUE,
+  seed = 123, stopping_rounds = 50, stopping_metric = "RMSE",
+  stopping_tolerance = 0
+)
 
-#Number randomely variable selected is mtry
-mtry <- sqrt(ncol(train.1))
-tunegrid <- expand.grid(.mtry=mtry)
+ensemble_tree_news <- h2o.stackedEnsemble(
+  x = X, y = Y, training_frame = train_h2o_news, model_id = "my_tree_ensemble",
+  base_models = list(best_gbm, best_xgb),
+  metalearner_algorithm = "drf"
+)
 
-rf_default <- train(topic~., 
-                    data=train.1, #uses downsampled, split train data
-                    method='rf', 
-                    metric='Accuracy', 
-                    tuneGrid=tunegrid, 
-                    trControl=control)
-print(rf_default)
+get_rmse <- function(model) {
+  results <- h2o.performance(model, newdata = test_h2o_news)
+  results@metrics$RMSE
+}
+list(best_gbm_news, best_xgb_news) %>%
+  purrr::map_dbl(get_rmse)
 
-# regularized regression
-library(glmnet)  
-
-train.1
-
-
-
+# Stacked results
+h2o.performance(ensemble_tree_news, newdata = test_h2o_news)@metrics$RMSE
