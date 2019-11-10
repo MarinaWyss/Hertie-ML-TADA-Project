c72d2c977426f63cf293ef97dba26adea3798ee1
Madeline Brady <madeline.n.brady@gmail.com>
2019-11-10 19:14:00 +0100

    Small Changes to Visuals
    still working

--- graphs/vizualizations.R
+++ graphs/vizualizations.R
@@ -2,26 +2,27 @@ library(caret)
 library(h2o)
 library(tidyverse)
 library(vip)
-library(wesanderson)
 
 h2o.init()   
 
 set.seed(123)  
 
-joinedDataSet <- read.csv("joinedDataSet.csv")
-joinedDataSet$topic <- as.factor(joinedDataSet$topic)
+data <- read.csv("preppedDataSet.csv")
+ideology <- read.csv("ideology.csv")
+data <- merge(data, ideology)
+
+data$topic <- as.factor(data$topic)
+data$X <- NULL
 
 # vizualizations
 
 ## overall top topic by outlet (before downsampling)
 ## calculate topic proportion for each outlet
-# breitbart: top topic is #1
-breitbartData <- joinedDataSet %>% filter(outlet == "breitbart")
+# breitbart: top topic is #2
+breitbartData <- data %>% filter(outlet == "breitbart")
 
-?scale_fill_manual
-
-ggplot(breitbartData, aes(x=factor(topic)))+
-  geom_bar(stat="count", width=0.7, fill = )
+plot1Topic <- ggplot(breitbartData, aes(x=topic, fill=topic))+
+  geom_bar(stat="count", width=0.7) + scale_fill_hue()
 
 breitbartCount <- as.data.frame(table(breitbartData$topic))
 breitbartCount %>% mutate(n = length(breitbartData$topic),
@@ -31,28 +32,37 @@ breitbartCount %>% mutate(n = length(breitbartData$topic),
 breitbartProp <- max(breitbartCount$Freq) / length(breitbartData$topic)
 
 
-# fox: top topic is #2
-foxData <- joinedDataSet %>% filter(outlet == "foxnews")
+# fox: top topic is #3
+foxData <- data %>% filter(outlet == "foxnews")
+
+plot2Topic <- ggplot(foxData, aes(x=topic, fill=topic))+
+  geom_bar(stat="count", width=0.7) + scale_fill_hue()
+plot2Topic
+
 foxCount <- as.data.frame(table(foxData$topic))
 foxProp <- max(foxCount$Freq) / length(foxData$topic)
 
 
 
 # nyt: top topic is #4
-nytData <- joinedDataSet %>% filter(outlet == "nytimes")
+nytData <- data %>% filter(outlet == "nytimes")
 nytCount <- as.data.frame(table(nytData$topic))
 nytProp <- max(nytCount$Freq) / length(nytData$topic)
 nytCount
 
 # wsj: top topic is #4
-wsjData <- joinedDataSet %>% filter(outlet == "wallstreetjournal")
+wsjData <- data %>% filter(outlet == "wallstreetjournal")
 wsjCount <- as.data.frame(table(wsjData$topic))
 wsjProp <- max(wsjCount$Freq) / length(wsjData$topic)
 
 # thinkprogress: top topic is #6
-thinkprogData <- joinedDataSet %>% filter(outlet == "thinkprogress")
+thinkprogData <- data %>% filter(outlet == "thinkprogress")
 thinkprogCount <- as.data.frame(table(thinkprogData$topic))
 thinkprogProp <- max(thinkprogCount$Freq) / length(thinkprogData$topic)
+
+# join plots
+grid.arrange(plot1, plot2, ncol=2)
+
 
 
 outlet_names <- c("breitbart", "foxnews", "nytimes", "wallstreetjournal", "thinkprogress")
