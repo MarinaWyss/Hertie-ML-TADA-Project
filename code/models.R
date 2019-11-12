6eccb13efd51d64ff8ed30ebb5caa80396583cd3
AlexandraMagaard <55913639+AlexandraMagaard@users.noreply.github.com>
2019-11-12 13:02:30 +0100

    updated models with tuning
    -added the RMSE and MSE for RF, GBM and stacked models
    -added new RF model with best parameters
    -tested updated RF model on test data
    -added code to get the best GBM model and determine RMSE
    -added code to run stacked model on train data
    -added code running stacked model on test data
    -calculated improved RMSE of stacked model on test data

--- code/models.R
+++ code/models.R
@@ -2,6 +2,7 @@ library(caret)
 library(h2o)
 library(tidyverse)
 
+
 h2o.init()   
 
 set.seed(123)  
@@ -19,7 +20,7 @@ balancedData <- upSample(x = data,
 
 # split data into training and test 
 index <- createDataPartition(data$topic, p = 0.7, 
-                               list = FALSE)
+                             list = FALSE)
 trainData <- data[index, ]
 testData  <- data[-index, ]
 
@@ -54,7 +55,11 @@ h2oRF1 <- h2o.randomForest(
   stopping_tolerance = 0
 )
 
-# hyperparameter tuning
+h2oRF1 
+# OOB RMSE is 0.7343532, around 73 percent
+# OOM MSE is 0.539
+
+# hyperparameter tuning to search a larger grid space for best possible model
 hyperGridRF <- list(
   mtries = c(-1, 1, 2, 3),
   min_rows = c(1, 3, 5, 10),
@@ -62,6 +67,7 @@ hyperGridRF <- list(
   sample_rate = c(.55, .632, .70, .80)
 )
 
+#set a random grid search to determine time constraints for finding best model
 searchCriteriaRF <- list(
   strategy = "RandomDiscrete",
   stopping_metric = "misclassification",
@@ -70,6 +76,7 @@ searchCriteriaRF <- list(
   max_runtime_secs = 60*10      # or cut grid search off at 10 minutes
 )
 
+#perform grid search
 randomGridRF <- h2o.grid(
   algorithm = "randomForest",
   grid_id = "rf_random_grid",
@@ -85,12 +92,64 @@ randomGridRF <- h2o.grid(
   search_criteria = searchCriteriaRF
 )
 
+# collect the results and sort by our model performance metric 
+# of choice
 gridPerformanceRF <- h2o.getGrid(
   grid_id = "rf_random_grid", 
   sort_by = "mean_per_class_error", 
   decreasing = FALSE
 )
 
+gridPerformanceRF
+#the grid assessed 140 models before stopping due to time
+#best model acheived mean per class error of: 0.7016590998715492, ~70 percent
+#best model has 10 max depth, 3 minimum rows, -1 mtries, 0.632 sample rate
+
+#adopting our RF to best hyperparameters
+# OOB model
+h2oRF2 <- h2o.randomForest(
+  x = predictors, 
+  y = response,
+  training_frame = trainH2o, 
+  ntrees = 1000, 
+  max_depth = 10, 
+  min_rows = 3, 
+  sample_rate = 0.632, 
+  nfolds = 10,
+  fold_assignment = "Modulo", 
+  keep_cross_validation_predictions = TRUE,
+  seed = 123, 
+  stopping_rounds = 50, 
+  stopping_metric = "misclassification",
+  stopping_tolerance = 0
+)
+
+h2oRF2
+#mean per class error is 0.7033
+#RMSE is 0.7343413
+# nearly the sampe RMSE and MSE as first random forest, but still
+
+# Now let’s evaluate the best hyperparameter model performance on a test set
+h2oRF3 <- h2o.randomForest(
+  x = predictors, 
+  y = response,
+  training_frame = testH2o, 
+  ntrees = 1000, 
+  max_depth = 10, 
+  min_rows = 3, 
+  sample_rate = 0.632, 
+  nfolds = 10,
+  fold_assignment = "Modulo", 
+  keep_cross_validation_predictions = TRUE,
+  seed = 123, 
+  stopping_rounds = 50, 
+  stopping_metric = "misclassification",
+  stopping_tolerance = 0
+)
+
+h2oRF3
+#RMSE on test set is: 0.7406932 - 0.01 improvement from training results
+#MSE on test set is: 0.5486265
 
 
 ### GBM ###
@@ -112,6 +171,17 @@ h2oGBM <- h2o.gbm(
   stopping_tolerance = 0
 )
 
+h2oGBM
+
+# model stopped after xx trees
+h2oGBM@parameters$ntrees
+## [1] 460
+
+# cross validated RMSE
+h2o.rmse(h2oGBM, xval = TRUE)
+## [1] 0.7377526 - slightly higher than random forest RMSE on training set
+
+
 # hyperparameter tuning
 hyperGridGBM <- list(
   sample_rate = c(0.5, 0.75, 1),              # row subsampling
@@ -119,20 +189,33 @@ hyperGridGBM <- list(
   col_sample_rate_per_tree = c(0.5, 0.75, 1)  # col subsampling for each tree
 )
 
+#random grid search strategy
 searchCriteriaGBM <- list(
   strategy = "RandomDiscrete",
   stopping_metric = "misclassification",
   stopping_tolerance = 0.001,   
   stopping_rounds = 10,         
-  max_runtime_secs = 60*60      
+  max_runtime_secs = 60*60    #one hour  
 )
 
+#perform grid search
 gridPerformanceGBM <- h2o.getGrid(
   grid_id = "gbm_grid", 
   sort_by = "mean_per_class_error", 
   decreasing = FALSE
 )
+gridPerformanceGBM
 
+
+# Grab the model_id for the top model, chosen by cross validation error
+best_model_id <- gridPerformanceGBM@model_ids[[1]]
+best_model <- h2o.getModel(best_model_id)
+
+# Now let’s get performance metrics on the best model
+h2o.performance(model = best_model, xval = TRUE)
+
+#what was the RMSE?
+#what was the MSE?
 
 ### STACKING MODELS ###
 # model
@@ -144,11 +227,30 @@ ensembleTree <- h2o.stackedEnsemble(
 
 # evaluation
 getMisclass <- function(model) {
+  results <- h2o.performance(model, newdata = trainH2o)
+  results@metrics$mean_per_class_error
+}
+list(h2oRF1, h2oGBM) %>%
+  purrr::map_dbl(getMisclass)
+#[1] 0.7033095 0.7058346
+#both random forest and GBM had equal RMSE scores
+
+#final evlauation metric
+h2o.performance(ensembleTree, newdata = trainH2o)@metrics$mean_per_class_error
+#[1] 0.7018815
+#stacked model gave us 0.02 percent performance gain with RMSE of 0.7018815
+
+# Run stacked model on test set
+
+# evaluation
+getMisclass <- function(model) {
   results <- h2o.performance(model, newdata = testH2o)
   results@metrics$mean_per_class_error
 }
 list(h2oRF1, h2oGBM) %>%
   purrr::map_dbl(getMisclass)
+#[1] 0.7181616 0.7181616 - GBM and RF perform equally
 
+#final evaluation metric
 h2o.performance(ensembleTree, newdata = testH2o)@metrics$mean_per_class_error
-
+#[1] 0.7162188 - improvement from training data!
